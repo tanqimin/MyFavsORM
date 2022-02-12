bf5a125b7a13b8b65c890e5d0c77c19e80631942
tanqimin <tanqimin@gmail.com>
2022-02-12 23:18:24 +0800

    refactor(example): 重构示例代码(支持多数据源)

--- framework.example/src/main/db/mysql.sql
+++ framework.example/src/main/db/mysql.sql
@@ -11,11 +11,34 @@
  Target Server Version : 80028
  File Encoding         : 65001
 
- Date: 11/02/2022 22:46:19
+ Date: 12/02/2022 23:17:19
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tenant
+-- ----------------------------
+DROP TABLE IF EXISTS `tenant`;
+CREATE TABLE `tenant`  (
+  `id` bigint NOT NULL,
+  `created` datetime NULL DEFAULT NULL,
+  `modified` datetime NULL DEFAULT NULL,
+  `tenant` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `jdbc_url` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `jdbc_user` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `jdbc_password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `jdbc_class` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tenant
+-- ----------------------------
+INSERT INTO `tenant` VALUES (1492150022916149249, '2022-02-12 21:29:20', '2022-02-12 21:29:22', 'master', 'jdbc:mysql://127.0.0.1:3306/myfavs_test?allowPublicKeyRetrieval=true&useUnicode=true&useServerPrepStmts=false&rewriteBatchedStatements=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8', 'root', 'root', 'com.mysql.cj.jdbc.Driver');
+INSERT INTO `tenant` VALUES (1492150022916149250, '2022-02-12 21:29:20', '2022-02-12 21:29:22', 'tenant1', 'jdbc:mysql://127.0.0.1:3306/tenant1?allowPublicKeyRetrieval=true&useUnicode=true&useServerPrepStmts=false&rewriteBatchedStatements=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8', 'root', 'root', 'com.mysql.cj.jdbc.Driver');
+INSERT INTO `tenant` VALUES (1492150022916149251, '2022-02-12 21:29:20', '2022-02-12 21:29:22', 'tenant2', 'jdbc:mysql://127.0.0.1:3306/tenant2?allowPublicKeyRetrieval=true&useUnicode=true&useServerPrepStmts=false&rewriteBatchedStatements=true&characterEncoding=utf-8&useSSL=false&serverTimezone=GMT%2B8', 'root', 'root', 'com.mysql.cj.jdbc.Driver');
 
 -- ----------------------------
 -- Table structure for user
@@ -35,5 +58,6 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES (1492150022916149248, '2022-02-11 12:00:00', '2022-02-11 12:00:01', 'master', 'karl_tam@126.com', '123456', 'ADMIN');
 
 SET FOREIGN_KEY_CHECKS = 1;
