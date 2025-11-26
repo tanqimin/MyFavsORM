effbfbcfb0899a262c81f48f4c736e2b25031e1a
tanqimin <tanqimin@gmail.com>
2025-11-26 11:30:17 +0800

    refactor(orm): 使用 Lombok 简化 DBConfig 类的 getter 方法
    - 为所有配置属性添加 @Getter 注解自动生成 getter 方法
    - 移除所有手动编写的 getter 方法
    - 保留原有的链式 setter 方法
    - 添加 Lombok 的 import 语句
    - 优化代码结构提高可读性

--- framework.orm/src/main/java/work/myfavs/framework/orm/DBConfig.java
+++ framework.orm/src/main/java/work/myfavs/framework/orm/DBConfig.java
@@ -1,5 +1,6 @@
 package work.myfavs.framework.orm;
 
+import lombok.Getter;
 import work.myfavs.framework.orm.meta.DbType;
 
 import java.sql.Connection;
@@ -11,83 +12,119 @@ import java.sql.Connection;
  */
 public class DBConfig {
 
-  public static final String   DEFAULT_DATASOURCE_NAME = "default";
+  public static final String  DEFAULT_DATASOURCE_NAME = "default";
   /**
    * 数据库类型
    */
-  private             String   dbType                  = DbType.MYSQL;
+  @Getter
+  private             String  dbType                  = DbType.MYSQL;
   /**
    * 一次批量插入数据的数量
    */
-  private             int      batchSize               = 200;
+  @Getter
+  private             int     batchSize               = 200;
   /**
    * 查询每次抓取数据的数量
+   * -- GETTER --
+   * 获取抓取数据大小
+   *
    */
-  private             int      fetchSize               = 1000;
+  @Getter
+  private             int     fetchSize               = 1000;
   /**
    * 是否显示SQL
    */
-  private             boolean  showSql                 = false;
+  private             boolean showSql                 = false;
   /**
    * 是否显示查询结果
    */
-  private             boolean  showResult              = false;
+  private             boolean showResult              = false;
   /**
    * 每页最大记录数
+   * -- GETTER --
+   * 获取分页时每页最大记录数
+   *
    */
-  private             int      maxPageSize             = -1;
+  @Getter
+  private             int     maxPageSize             = -1;
   /**
    * 默认事务级别
+   * -- GETTER --
+   * 获取默认事务隔离级别
+   *
    */
-  private             int      defaultIsolation        = Connection.TRANSACTION_READ_COMMITTED;
+  @Getter
+  private             int     defaultIsolation        = Connection.TRANSACTION_READ_COMMITTED;
   /**
    * 终端ID
+   * -- GETTER --
+   * 获取终端ID
+   *
    */
-  private             long     workerId                = 1L;
+  @Getter
+  private             long    workerId                = 1L;
   /**
    * 数据中心ID
+   * -- GETTER --
+   * 获取数据中心ID
+   *
    */
-  private             long     dataCenterId            = 1L;
+  @Getter
+  private             long    dataCenterId            = 1L;
 
   /**
    * 分页查询结果数据集合字段名称
+   * -- GETTER --
+   * 获取分页查询结果数据集合字段名称
+   *
    */
+  @Getter
   private String pageDataField = "data";
 
   /**
    * 分页查询结果当前页码字段名称
+   * -- GETTER --
+   * 获取分页查询结果当前页码字段名称
+   *
    */
+  @Getter
   private String pageCurrentField = "currentPage";
 
   /**
    * 分页查询结果每页记录数字段名称
+   * -- GETTER --
+   * 获取分页查询结果每页记录数字段名称
+   *
    */
+  @Getter
   private String pageSizeField = "pageSize";
 
   /**
    * 分页查询结果总页数字段名称
+   * -- GETTER --
+   * 获取分页查询结果总页数字段名称
+   *
    */
+  @Getter
   private String pageTotalPageField = "totalPages";
 
   /**
    * 分页查询结果总记录数字段名称
+   * -- GETTER --
+   * 获取分页查询结果总记录数字段名称
+   *
    */
+  @Getter
   private String pageTotalRecordField = "totalRecords";
 
   /**
    * 分页查询结果是否存在下一页字段名称
-   */
-  private String pageHasNextField = "hasNext";
-
-  /**
-   * 获取数据库类型
+   * -- GETTER --
+   * 获取分页查询结果是否存在下一页字段名称
    *
-   * @return 数据库类型
    */
-  public String getDbType() {
-
-    return dbType;
-  }
+  @Getter
+  private String pageHasNextField = "hasNext";
 
   /**
    * 设置数据库类型
@@ -102,16 +139,6 @@ public class DBConfig {
   }
 
   /**
-   * 获取批处理大小
-   *
-   * @return 批处理大小
-   */
-  public int getBatchSize() {
-
-    return batchSize;
-  }
-
-  /**
    * 设置批处理大小
    *
    * @param batchSize 批处理大小
@@ -121,16 +148,6 @@ public class DBConfig {
 
     this.batchSize = batchSize;
     return this;
-  }
-
-  /**
-   * 获取抓取数据大小
-   *
-   * @return 抓取数据大小
-   */
-  public int getFetchSize() {
-
-    return fetchSize;
   }
 
   /**
@@ -190,16 +207,6 @@ public class DBConfig {
   }
 
   /**
-   * 获取分页时每页最大记录数
-   *
-   * @return 分页时每页最大记录数
-   */
-  public int getMaxPageSize() {
-
-    return maxPageSize;
-  }
-
-  /**
    * 设置分页时每页最大记录数(小于 0 为不限制)
    *
    * @param maxPageSize 分页时每页最大记录数
@@ -209,16 +216,6 @@ public class DBConfig {
 
     this.maxPageSize = maxPageSize;
     return this;
-  }
-
-  /**
-   * 获取默认事务隔离级别
-   *
-   * @return int
-   */
-  public int getDefaultIsolation() {
-
-    return this.defaultIsolation;
   }
 
   /**
@@ -234,16 +231,6 @@ public class DBConfig {
   }
 
   /**
-   * 获取终端ID
-   *
-   * @return 终端ID
-   */
-  public long getWorkerId() {
-
-    return workerId;
-  }
-
-  /**
    * 设置终端ID
    *
    * @param workerId 终端ID
@@ -253,16 +240,6 @@ public class DBConfig {
 
     this.workerId = workerId;
     return this;
-  }
-
-  /**
-   * 获取数据中心ID
-   *
-   * @return 数据中心ID
-   */
-  public long getDataCenterId() {
-
-    return dataCenterId;
   }
 
   /**
@@ -278,15 +255,6 @@ public class DBConfig {
   }
 
   /**
-   * 获取分页查询结果数据集合字段名称
-   *
-   * @return 分页查询结果数据集合字段名称
-   */
-  public String getPageDataField() {
-    return pageDataField;
-  }
-
-  /**
    * 设置分页查询结果数据集合字段名称
    *
    * @param pageDataField 分页查询结果数据集合字段名称
@@ -295,15 +263,6 @@ public class DBConfig {
   public DBConfig setPageDataField(String pageDataField) {
     this.pageDataField = pageDataField;
     return this;
-  }
-
-  /**
-   * 获取分页查询结果当前页码字段名称
-   *
-   * @return 分页查询结果当前页码字段名称
-   */
-  public String getPageCurrentField() {
-    return pageCurrentField;
   }
 
   /**
@@ -318,15 +277,6 @@ public class DBConfig {
   }
 
   /**
-   * 获取分页查询结果每页记录数字段名称
-   *
-   * @return 分页查询结果每页记录数字段名称
-   */
-  public String getPageSizeField() {
-    return pageSizeField;
-  }
-
-  /**
    * 设置分页查询结果每页记录数字段名称
    *
    * @param pageSizeField 分页查询结果每页记录数字段名称
@@ -335,15 +285,6 @@ public class DBConfig {
   public DBConfig setPageSizeField(String pageSizeField) {
     this.pageSizeField = pageSizeField;
     return this;
-  }
-
-  /**
-   * 获取分页查询结果总页数字段名称
-   *
-   * @return 分页查询结果总页数字段名称
-   */
-  public String getPageTotalPageField() {
-    return pageTotalPageField;
   }
 
   /**
@@ -358,15 +299,6 @@ public class DBConfig {
   }
 
   /**
-   * 获取分页查询结果总记录数字段名称
-   *
-   * @return 分页查询结果总记录数字段名称
-   */
-  public String getPageTotalRecordField() {
-    return pageTotalRecordField;
-  }
-
-  /**
    * 设置分页查询结果总记录数字段名称
    *
    * @param pageTotalRecordField 分页查询结果总记录数字段名称
@@ -375,15 +307,6 @@ public class DBConfig {
   public DBConfig setPageTotalRecordField(String pageTotalRecordField) {
     this.pageTotalRecordField = pageTotalRecordField;
     return this;
-  }
-
-  /**
-   * 获取分页查询结果是否存在下一页字段名称
-   *
-   * @return 分页查询结果是否存在下一页字段名称
-   */
-  public String getPageHasNextField() {
-    return pageHasNextField;
   }
 
   /**
